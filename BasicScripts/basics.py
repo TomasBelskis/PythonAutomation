d5381a8274d070cdedd6a4e2b3849c4587b27a73
TomasLT <tomas.belskis@icloud.com>
2019-08-04 18:09:50 +0100

    Basic python refresh

---
+++ BasicScripts/basics.py
@@ -0,0 +1,16 @@
+# Python Basics
+
+# String concatenaton
+added_strings =  str(32) + "_342"
+
+# Getting input
+input_from_user = input()
+
+# Basic print function
+print(input_from_user)
+
+# Mixing boolean and comparison operations
+
+if (4 < 5) and (5 < 6):
+    print("True")
+
